3b16acca33bb829ed610f28fe3b4a1e5d3a68d7b
Mekin Baturalp <79316684+kayamekin@users.noreply.github.com>
2022-08-08 05:38:42 +0300

    login ui and video background added

--- lib/pages/netflix/homepage.dart
+++ lib/pages/netflix/homepage.dart
@@ -10,25 +10,135 @@ class NetflixHomePage extends StatefulWidget {
 
 class _NetflixHomePageState extends State<NetflixHomePage> {
   late var size = MediaQuery.of(context).size;
-  double heightLoginButton = 700;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: SingleChildScrollView(
+      body: SafeArea(
+        child: SingleChildScrollView(
+            child: Expanded(
           child: Stack(
-        children: [
-          Center(
-            child: GestureDetector(
-              onTap: () {
-                setState(() {
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => LoginPageNetflix()));
-                });
-              },
-              child: Container(
-                height: heightLoginButton,
+            children: [
+              BackgroundAndLogoStyle(),
+              LoginPageRouteButton(context),
+            ],
+          ),
+        )),
+      ),
+    );
+  }
+
+  Stack BackgroundAndLogoStyle() {
+    return Stack(
+      children: [
+        Container(
+          width: size.height - 80,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Stack(
+                children: [
+                  Expanded(
+                    child: Container(
+                      height: size.height,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage(
+                            'assets/image/backgroundimage.jpg',
+                          ),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: size.height,
+                    color: Colors.black.withAlpha(120),
+                  ),
+                  Container(
+                    height: size.height,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          Color(0xff030303).withOpacity(0.85),
+                          Color(0xff030303).withOpacity(0)
+                        ],
+                        end: Alignment.topCenter,
+                        begin: Alignment.bottomCenter,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        Column(
+          children: [
+            SizedBox(
+              height: 250,
+            ),
+            Center(
+              child: Image.asset(
+                'assets/image/netflix_logo.png',
+                fit: BoxFit.cover,
+                width: size.width / 3 * 2,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Column LoginPageRouteButton(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(height: 400),
+        Center(
+          child: GestureDetector(
+            onTap: () {
+              setState(() {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => LoginPageNetflix()));
+              });
+            },
+            child: Stack(children: [
+              // Container(
+              //   height: 150,
+              //   width: size.width / 3 * 2,
+              //   decoration: BoxDecoration(
+              //       color: Colors.red.shade500,
+              //       boxShadow: [
+              //         BoxShadow(
+              //           color: Colors.red.withAlpha(40),
+              //           offset: Offset(0, 32),
+              //           blurRadius: 32.0,
+              //           spreadRadius: 32,
+              //         ),
+              //       ],
+              //       borderRadius: BorderRadius.only(
+              //         bottomRight: Radius.circular(10),
+              //         bottomLeft: Radius.circular(10),
+              //       )),
+              // ),
+              Container(
+                width: size.width / 3 * 2,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(10),
+                    bottomRight: Radius.circular(10),
+                  ),
+                  gradient: LinearGradient(colors: [
+                    Colors.black.withAlpha(200),
+                    Colors.white.withAlpha(0),
+                  ], end: Alignment.topRight, begin: Alignment.bottomLeft),
+                ),
+                height: 150,
+              ),
+              Container(
+                height: 120,
                 width: size.width / 3 * 2,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.end,
@@ -48,77 +158,14 @@ class _NetflixHomePageState extends State<NetflixHomePage> {
                     ),
                     SizedBox(
                       height: 15,
-                    )
+                    ),
                   ],
                 ),
-                decoration: BoxDecoration(
-                    color: Colors.red.shade500,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.red.withAlpha(40),
-                        offset: Offset(0, 32),
-                        blurRadius: 32.0,
-                        spreadRadius: 24,
-                      ),
-                    ],
-                    borderRadius: BorderRadius.only(
-                      bottomRight: Radius.circular(10),
-                      bottomLeft: Radius.circular(10),
-                    )),
               ),
-            ),
+            ]),
           ),
-          Container(
-              width: size.height - 80,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Stack(
-                    children: [
-                      Container(
-                        height: 500,
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                            image: AssetImage(
-                              'assets/image/backgroundimage.jpg',
-                            ),
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                      Container(
-                        height: 500,
-                        decoration: BoxDecoration(
-                          gradient: LinearGradient(
-                            colors: [
-                              Color(0xff030303).withOpacity(0.85),
-                              Color(0xff030303).withOpacity(0)
-                            ],
-                            end: Alignment.topCenter,
-                            begin: Alignment.bottomCenter,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              )),
-          Column(
-            children: [
-              SizedBox(
-                height: 100,
-              ),
-              Center(
-                child: Image.asset(
-                  'assets/image/netflix_logo.png',
-                  fit: BoxFit.cover,
-                  width: size.width / 2,
-                ),
-              ),
-            ],
-          ),
-        ],
-      )),
+        ),
+      ],
     );
   }
 }
--- lib/pages/netflix/loginpage.dart
+++ lib/pages/netflix/loginpage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+import 'package:chewie/chewie.dart';
 
 class LoginPageNetflix extends StatefulWidget {
   const LoginPageNetflix({Key? key}) : super(key: key);
@@ -10,85 +12,130 @@ class LoginPageNetflix extends StatefulWidget {
 class _LoginPageNetflixState extends State<LoginPageNetflix> {
   late var size = MediaQuery.of(context).size;
 
+  VideoPlayerController videoPlayerController =
+      VideoPlayerController.asset('assets/videos/netflix_intro.mp4');
+  ChewieController? chewieController;
+
+  @override
+  void initState() {
+    chewieController = ChewieController(
+      videoPlayerController: videoPlayerController,
+      aspectRatio: 12 / 24,
+      autoPlay: true,
+      looping: false,
+      autoInitialize: true,
+      showControls: false,
+      allowMuting: false,
+    );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    videoPlayerController.dispose();
+    chewieController!.dispose();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back_ios),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-        ),
-      ),
+      backgroundColor: Colors.black54,
       body: SingleChildScrollView(
-        child: Stack(
-          children: [
-            Center(
-              child: Container(
-                child: Image.asset(
-                  'assets/image/netflix_logo.png',
-                  width: 300,
+        child: SafeArea(
+          child: Stack(
+            children: [
+              Container(
+                height: size.height,
+                child: Chewie(
+                  controller: chewieController!,
                 ),
               ),
-            ),
-            Container(
-              height: 300,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Center(
-                    child: Container(
-                      width: 300,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: TextFormField(
-                          decoration: InputDecoration(
-                            enabledBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Colors.white.withAlpha(240),
-                                width: 2,
+              Container(
+                color: Colors.black54,
+                width: double.infinity,
+                height: size.height,
+              ),
+              Positioned(
+                top: 200,
+                width: MediaQuery.of(context).size.width,
+                child: Material(
+                  color: Colors.transparent,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Center(
+                        child: Container(
+                          child: Image.asset(
+                            'assets/image/netflix_logo.png',
+                            width: size.height / 3,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: 300,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Center(
+                              child: Container(
+                                width: 300,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: TextFormField(
+                                    decoration: InputDecoration(
+                                      enabledBorder: UnderlineInputBorder(
+                                        borderSide: BorderSide(
+                                          color: Colors.white.withAlpha(240),
+                                          width: 2,
+                                        ),
+                                      ),
+                                      labelText: 'Email',
+                                      labelStyle: TextStyle(
+                                          color: Colors.white, fontSize: 22),
+                                    ),
+                                  ),
+                                ),
                               ),
                             ),
-                            labelText: 'Email',
-                            labelStyle:
-                                TextStyle(color: Colors.white, fontSize: 22),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Center(
-                    child: Container(
-                      width: 300,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: TextFormField(
-                          obscureText: true,
-                          decoration: InputDecoration(
-                            enabledBorder: UnderlineInputBorder(
-                              borderSide:
-                                  BorderSide(color: Colors.white, width: 2),
+                            Center(
+                              child: Container(
+                                width: 300,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: TextFormField(
+                                    obscureText: true,
+                                    decoration: InputDecoration(
+                                      fillColor: Colors.transparent,
+                                      enabledBorder: UnderlineInputBorder(
+                                        borderSide: BorderSide(
+                                            color: Colors.white, width: 2),
+                                      ),
+                                      labelText: 'Password',
+                                      labelStyle:
+                                          TextStyle(color: Colors.white),
+                                    ),
+                                  ),
+                                ),
+                              ),
                             ),
-                            labelText: 'Password',
-                            labelStyle: TextStyle(color: Colors.white),
-                          ),
+                          ],
                         ),
                       ),
-                    ),
+                      // Container(
+                      //     width: double.infinity,
+                      //     height: size.height - 120,
+                      //     child: GoLogin()),
+                    ],
                   ),
-                ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
-      floatingActionButton: GoLogin(),
     );
   }
 }
@@ -107,28 +154,34 @@ class GoLogin extends StatelessWidget {
           onTap: () {
             Navigator.of(context).pop();
           },
+          child: Expanded(
+            flex: 1,
+            child: Container(
+              width: size.width / 2,
+              height: 80,
+              child: Center(
+                child: Icon(Icons.arrow_back_ios),
+              ),
+              color: Color.fromARGB(255, 255, 255, 255),
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 1,
           child: Container(
             width: size.width / 2,
             height: 80,
             child: Center(
-              child: Icon(Icons.arrow_back_ios),
-            ),
-            color: Color.fromARGB(255, 255, 255, 255),
+                child: Text(
+              "Submit",
+              style: TextStyle(
+                color: Colors.white.withAlpha(200),
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            )),
+            color: Color.fromARGB(255, 236, 45, 45),
           ),
-        ),
-        Container(
-          width: size.width / 2,
-          height: 80,
-          child: Center(
-              child: Text(
-            "Submit",
-            style: TextStyle(
-              color: Colors.white.withAlpha(200),
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-            ),
-          )),
-          color: Color.fromARGB(255, 236, 45, 45),
         ),
       ],
     );
